76ab7b34fc92acda15e97e9072ef21e2b67e76fb
Shakil Ahmad <ahmad.shakil444@gmail.com>
2022-08-24 20:08:21 +0530

    first comit shakil

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Provider  store={store}>
   <React.StrictMode>
-        <App />
-      
-      </React.StrictMode>
-      </Provider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ChakraProvider>
     </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

ae57f8084613dcc5aa0f4ce832636e2d31e52ecb
vin9012d <vin9012c@gmail.com>
2022-08-24 09:05:39 +0530

    chakra setup done in indexjs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
@@ -11,10 +12,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider  store={store}>
+      <ChakraProvier>
   <React.StrictMode>
         <App />
       
-      </React.StrictMode>
+        </React.StrictMode>
+        </ChakraProvier>
       </Provider>
     </BrowserRouter>
 );

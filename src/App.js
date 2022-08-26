bdfb191b429a176acbc964d0f4561bbbb99d2239
vin9012d <vin9012c@gmail.com>
2022-08-26 09:23:31 +0530

    day-3 f submit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 
 import LineChart from "./components/Test";
+import { Addcannel } from "./pages/Addcannel";
 import { Analytics } from "./pages/Analytics";
 import { Channels } from "./pages/Channels";
 import { Mainroute } from "./pages/Mainroute";
@@ -8,6 +9,7 @@ import { Mainroute } from "./pages/Mainroute";
 function App() {
   return (
     <div className="App">
+      <Addcannel/>
       <Channels />
       <LineChart />
       <Analytics />

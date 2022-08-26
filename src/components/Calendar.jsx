83cd795a36104df3c79c802c589765cf54933fa2
soumya25 <soumyanil22@gmail.com>
2022-08-26 23:28:56 +0530

    added post creating functionality

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -6,7 +6,21 @@ import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Select,
+} from "@chakra-ui/react";
 
 const locales = {
   "en-In": require("date-fns/locale/en-IN"),
@@ -39,14 +53,53 @@ const events = [
 ];
 
 const CalendarComponent = () => {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleAddEvent = () => {
+    setAllEvents([...allEvents, newEvent]);
+  };
+
   return (
-    <Box w="85%">
+    <Box w="85%" mt="30px">
+      <Button
+        variant="solid"
+        colorScheme="messenger"
+        borderRadius="3px"
+        size="md"
+        position="absolute"
+        right="21%"
+        mt="-2px"
+        onClick={onOpen}
+      >
+        Create Post
+      </Button>
+      <Modal size="2xl" isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader borderBottom="1px solid grey">
+            <Flex
+              alignItems="center"
+              ml="5px"
+              mr="5px"
+              justifyContent="space-between"
+            >
+              <Text fontSize="sm">Campaign</Text>
+              <Box w="280px">
+                <Select borderRadius="3px" placeholder="No Campaign" />
+              </Box>
+            </Flex>
+          </ModalHeader>
+          <ModalBody></ModalBody>
+        </ModalContent>
+      </Modal>
       <Calendar
         localizer={localizer}
-        events={events}
+        events={allEvents}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: "100vh" }}
+        style={{ height: "83vh" }}
       />
     </Box>
   );

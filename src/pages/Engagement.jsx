efc41b04b309c1c1463df9a02fe03f5d7dd26009
rs230498 <singhrobin2304@gmail.com>
2022-08-27 23:43:19 +0530

    robin day5

--- src/pages/Engagement.jsx
+++ src/pages/Engagement.jsx
@@ -8,12 +8,13 @@ import { BsFillHeartFill, } from "react-icons/bs";
 import { MdChat } from "react-icons/md";
 import { PopoverTrigger, PopoverContent, PopoverHeader, PopoverBody, PopoverFooter, Popover, PopoverArrow, PopoverCloseButton, PopoverAnchor } from '@chakra-ui/react'
 import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react'
+import AltNavbar from '../components/AltNavbar'
 
 export const Engagement = () => {
   const ref = useRef(null)
   const [cid, scid] = useState(1)
-  const [user,suser] = useState(engagementdata)
-  const [acc,sacc] = useState("Animals_hd_pic!")
+  const [user, suser] = useState(engagementdata)
+  const [acc, sacc] = useState("Animals_hd_pic!")
   const [data, sdata] = useState({})
   const [a, sa] = useState(-1)
   const [b, sb] = useState(-1)
@@ -36,7 +37,7 @@ export const Engagement = () => {
   }
   function Show() {
     return (
-      <Box  width="100%">
+      <Box width="100%">
         {data.commentsno > 0 && data.comments.map((el, index) => {
           return (
             <Accordion key={index} allowToggle borderBottom={"none"}>
@@ -49,28 +50,28 @@ export const Engagement = () => {
                         <Heading fontSize={"22px"} color="white">{el.id[0].toUpperCase()}</Heading>
                       </Box>
                     </Box>
-                    <Box  width={"100%"} height="100%" textAlign={"left"} overflow="hidden" >
+                    <Box width={"100%"} height="100%" textAlign={"left"} overflow="hidden" >
                       <Box display={"flex"}><Text fontWeight={"bold"}>&nbsp;&nbsp;&nbsp;{el.id}&nbsp;&nbsp;&nbsp;&nbsp;</ Text><Text>{el.comment}</Text></Box>
                       <Text fontSize={"12px"}>&nbsp;&nbsp;&nbsp;{el.days}</Text>
-                      {el.rep?<Text fontSize={"10px"} fontWeight={"bold"} mt="2%">&nbsp;&nbsp;&nbsp;---View Reply</Text>:""}
+                      {el.rep ? <Text fontSize={"10px"} fontWeight={"bold"} mt="2%">&nbsp;&nbsp;&nbsp;---View Reply</Text> : ""}
                     </Box>
-                    
+
                   </AccordionButton>
                 </h2>
-                {el.rep?<AccordionPanel pb={4} >
-                <Box display={"flex"} flexDirection="row-reverse">
-                      <Box borderRadius="50%" backgroundColor={"rgb(184,184,184)"} padding="5px 13px" mt={"0px"} textAlign={"right"}>
-                        <Heading fontSize={"22px"} color="white" textAlign={"right"}>{el.id[0].toUpperCase()}</Heading>
-                      </Box>
-                      <Box textAlign={"right"}>
-                      <Box display={"flex"}>
-                      <Text>{el.rep}</Text><Text fontWeight={"bold"}>&nbsp;&nbsp;&nbsp;{el.id}&nbsp;&nbsp;&nbsp;&nbsp;</ Text>
-                      </Box>
-                  <Text fontSize={"12px"}>&nbsp;&nbsp;&nbsp;{el.rdays}&nbsp;&nbsp;&nbsp;</Text>
-                  </Box>
+                {el.rep ? <AccordionPanel pb={4} >
+                  <Box display={"flex"} flexDirection="row-reverse">
+                    <Box borderRadius="50%" backgroundColor={"rgb(184,184,184)"} padding="5px 13px" mt={"0px"} textAlign={"right"}>
+                      <Heading fontSize={"22px"} color="white" textAlign={"right"}>{acc[0].toUpperCase()}</Heading>
                     </Box>
-                  
-                </AccordionPanel>:""}
+                    <Box textAlign={"right"}>
+                      <Box display={"flex"}>
+                        <Text>{el.rep}</Text><Text fontWeight={"bold"}>&nbsp;&nbsp;&nbsp;{acc}&nbsp;&nbsp;&nbsp;&nbsp;</ Text>
+                      </Box>
+                      <Text fontSize={"12px"}>&nbsp;&nbsp;&nbsp;{el.rdays}&nbsp;&nbsp;&nbsp;</Text>
+                    </Box>
+                  </Box>
+
+                </AccordionPanel> : ""}
               </AccordionItem>
             </Accordion>
           )
@@ -79,30 +80,62 @@ export const Engagement = () => {
     )
   }
 
-  function a1(){
+  function a1() {
     suser(engagementdata)
-    sacc("Animals_hd_pic!")
+    sacc("Animal_hd_world@565")
   }
-  function a2(){
+  function a2() {
     suser(engagementdata1)
-    sacc("Nature_hd_pic!")
+    sacc("Nature_hd_world@565")
   }
   useEffect(() => {
     handleeclick(1)
   }, [])
   return (
-    <Box display={"flex"}>
-      <Box height={"90vh"} width={"36%"} border={"0.1px solid grey"} cursor="pointer">
+    <Box>
+      <AltNavbar/>
+    <Box display={{base: 'block', // 0-48em
+    md: 'flex', // 48em-80em,
+    xl: 'flex'}}>
+      <Box height={{base: '60vh',md: '90vh', xl: '90vh'}} width={{base: '100%',md: '40%', xl: '36%'}} border={"0.1px solid grey"} cursor="pointer">
         <Box height={"10%"} borderBottom={"0.1px solid grey"} display="flex" justifyContent={"center"} alignItems="center">
           <Box zIndex={"222"} width="50%" margin={"auto"} >
             <Popover width={"100%"}>
               <PopoverTrigger width={"100%"}>
-                <Button width={"100%"} >{acc}</Button>
+                <Button width={"100%"} >
+                <Box display={"flex"} justifyContent="center" alignItems={"center"}>
+                    <Box borderRadius="50%" backgroundColor={"rgb(184,184,184)"} padding="2.5px 6.5px" mt={"0px"} textAlign={"center"}>
+                      <Heading fontSize={"18px"} color="white" textAlign={"right"}>{acc[0].toUpperCase()}</Heading>
+                    </Box>
+                    <Box textAlign={"right"}>
+                      <Box display={"flex"}><Text fontWeight={"bold"}>&nbsp;&nbsp;&nbsp;{acc}&nbsp;&nbsp;&nbsp;&nbsp;</ Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Button>
               </PopoverTrigger>
               <PopoverContent>
                 <PopoverArrow />
-                <PopoverHeader onClick={a1} >Animals_hd_pic!</PopoverHeader>
-                <PopoverHeader onClick={a2}>Nature_hd_pics</PopoverHeader>
+                <PopoverHeader onClick={a1}><Box display={"flex"} justifyContent="center" alignItems={"center"}>
+                    <Box borderRadius="50%" backgroundColor={"rgb(184,184,184)"} padding="2.5px 6.5px" mt={"0px"} textAlign={"center"}>
+                      <Heading fontSize={"18px"} color="white" textAlign={"right"}>A</Heading>
+                    </Box>
+                    <Box textAlign={"right"}>
+                      <Box display={"flex"}><Text fontWeight={"bold"}>&nbsp;Animal_hd_world@565</ Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                  </PopoverHeader>
+                <PopoverHeader onClick={a2}><Box display={"flex"} justifyContent="center" alignItems={"center"}>
+                    <Box borderRadius="50%" backgroundColor={"rgb(184,184,184)"} padding="2.5px 6.5px" mt={"0px"} textAlign={"center"}>
+                      <Heading fontSize={"18px"} color="white" textAlign={"right"}>N</Heading>
+                    </Box>
+                    <Box textAlign={"right"}>
+                      <Box display={"flex"}><Text fontWeight={"bold"}>&nbsp;Nature_hd_world@565</ Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                  </PopoverHeader>
               </PopoverContent>
             </Popover>
           </Box>
@@ -123,7 +156,7 @@ export const Engagement = () => {
           })}
         </Box>
       </Box>
-      <Box height={"88vh"} borderRadius={"5px"} border={"0.1px solid grey"} width={"40%"} marginLeft={"10%"} marginTop={"1vh"}>
+      <Box height={"86vh"} borderRadius={"5px"} border={"0.1px solid grey"} width={{base: '100%',md: '40%', xl: '40%'}} marginLeft={{base: '0%',md: '10%', xl: '10%'}} marginTop={"1vh"}>
         <Box height={"20%"} borderBottom={"0.1px solid grey"} display="flex" alignItems="center">
           <Box width={"20%"} height="90%" borderRadius={"7px"} ml="10px" >
             <img style={{ borderRadius: "7px", width: "100%", height: "100%" }} src={data.imgurl} alt="" />
@@ -140,24 +173,9 @@ export const Engagement = () => {
         {data.commentsno > 0 ? <Show /> : <Comments />}
       </Box>
     </Box>
-
+    </Box>       
   )
 }
 
 
 
-// <Popover width="100%" key={index}>
-//           <PopoverTrigger width="100%">
-//             <Button width={"97%"} margin="auto" backgroundColor={"rgb(224,224,224)"} height="13vh" mt={"1vh"}
-//               borderRadius="5px">
-//                 <Box display={"flex"}>
-//                   <Box ><Heading>{el.id[0].toUpperCase()}</Heading></Box>
-//                   <Box></Box>
-//                 </Box>
-//               </Button>
-//           </PopoverTrigger>
-//           <PopoverContent width={"337%"} margin="auto" backgroundColor={"rgb(224,224,224)"} height="8vh" mt={"1vh"}
-//               borderRadius="5px" ml={"-73%"}>
-//             <PopoverHeader>Confirmation!</PopoverHeader>
-//           </PopoverContent>
-//         </Popover>
--- src/redux/Authreducer/reducer.js
+++ src/redux/Authreducer/reducer.js
@@ -1,8 +1,31 @@
-const initstate = {
-    isLoading:false
-}
+import * as data from "./actiontype"
+let init = {
+    token : "",
+    isAuth:false,
+    isAuthloading:false
 
-export const reducer = (state = initstate, action) => {
-    let { type, payload } = action;
-    return state;
+}
+export const reducer = (oldstate = init,action)=>{
+    
+    const {type,payload} = action
+    switch (type) {
+        case data.USER_LOGIN_REQUEST:
+            return {
+                ...oldstate,isAuth:false,isAuthloading:true
+            };
+        case data.USER_LOGIN_SUCCESS:
+            return {
+                ...oldstate,isAuth:true,isAuthloading:false,token:payload
+            };
+        case data.USER_LOGIN_FAILURE:
+            return {
+                ...oldstate,isAuthloading:false,isAuth:false
+            };
+
+        case data.USER_LOGOUT_SUCCESS:
+            return {
+                ...oldstate,isAuthloading:false,isAuth:false,token:""
+            }
+        default:return oldstate
+    }
 }
